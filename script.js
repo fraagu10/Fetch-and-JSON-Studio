83ea0e876b0aea884d84760a3eeb86b331ccb66a
Franco <aguilarfranco1092@protonmail.com>
2022-08-22 14:43:11 -0500

    Added information from fetched API into the HTML elements in order for the astronauts to be displayed.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then( function(astronauts) {
+            const astronautBox = document.getElementById("container");
+            
+            for(i = 0; i < astronauts.length; i++) {
+                astronautBox.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${astronauts[i].firstName} ${astronauts[i].lastName}</h3>
+                                <ul>
+                                    <li>Hours in space: ${astronauts[i].hoursInSpace} </li>
+                                    <li>Active: ${astronauts[i].active} </li>
+                                    <li>Skills: ${astronauts[i].skills} </li>
+                                </ul>
+                        </div>
+                        <img class="avatar" src=${astronauts[i].picture}>
+                    </div>
+                `;
+            }
+        });
+    });
+});
